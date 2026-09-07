386977c5022704154e9cd1900735ac76f64b6a4b
Lexer <89482099+Lexer-Lux@users.noreply.github.com>
2026-09-06 21:57:01 -0600

    FF8 #327: expose freed Square edge for Flare

--- games/ff8/ffnx_modern_controls/lexeditor_ff8_modern_controls.h
+++ games/ff8/ffnx_modern_controls/lexeditor_ff8_modern_controls.h
@@ -4,4 +4,5 @@
 void lexeditor_ff8_modern_controls_install();
 bool lexeditor_ff8_modern_controls_world_active();
 bool lexeditor_ff8_modern_controls_battle_active();
+bool lexeditor_ff8_modern_controls_take_square_press();
 int lexeditor_ff8_modern_world_axis(std::int8_t port, int type, std::int8_t offset);
