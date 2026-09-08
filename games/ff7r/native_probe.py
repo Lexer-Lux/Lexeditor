1e6f36d19dd2157ea456c11e2c614cb3ffc81e70
Lexer <89482099+Lexer-Lux@users.noreply.github.com>
2026-09-08 00:57:15 -0600

    Probe FF7R state-trigger API anchors for minimap research

--- games/ff7r/native_probe.py
+++ games/ff7r/native_probe.py
@@ -29,6 +29,8 @@ DEFAULT_NEEDLES = (
     "HideNavimap",
     "BPShowNavimap",
     "BPHideNavimap",
+    "SendStateTrigger",
+    "SendStateTriggerDirect",
     "trgCmn_NaviMap_Update_On",
     "trgCmn_NaviMap_Update_Off",
     "BPSetPlayerHPMax",
