52f502b7d0d40191c7e9a89fe395e97a94ab2c2a
Lexer <89482099+Lexer-Lux@users.noreply.github.com>
2026-09-09 07:39:24 -0600

    Run Palworld build-capable service

--- runtime_bootstrap.py
+++ runtime_bootstrap.py
@@ -8,7 +8,7 @@ import sys
 
 SERVICE_MODULES = frozenset({
     'games.blank.server', 'games.ff7.server', 'games.ff7r.server', 'games.ff7r.themed_server',
-    'games.ff8.server', 'games.ff9.server', 'games.palworld.server', 'games.rdr.server', 'games.rdr2.server',
+    'games.ff8.server', 'games.ff9.server', 'games.palworld.build_server', 'games.rdr.server', 'games.rdr2.server',
     'games.warband.server',
 })
 
