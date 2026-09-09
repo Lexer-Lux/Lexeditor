b461d03cb080ee54040cad20fef721313bfcd77b
Lexer <89482099+Lexer-Lux@users.noreply.github.com>
2026-09-09 00:01:03 -0600

    feat(chrono-trigger): allow managed plugin service

--- runtime_bootstrap.py
+++ runtime_bootstrap.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 
 SERVICE_MODULES = frozenset({
-    'games.blank.server', 'games.ff7.server', 'games.ff8.server',
+    'games.blank.server', 'games.chrono_trigger.server', 'games.ff7.server', 'games.ff8.server',
     'games.ff9.server', 'games.rdr.server', 'games.rdr2.server', 'games.warband.server',
 })
 
