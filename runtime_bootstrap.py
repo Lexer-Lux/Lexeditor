1c9e1e21586de04fc98e08d2ef49edc4edba2786
Lexer <89482099+Lexer-Lux@users.noreply.github.com>
2026-09-12 02:08:30 -0600

    Allow Terraria plugin service in installed runtime

--- runtime_bootstrap.py
+++ runtime_bootstrap.py
@@ -9,7 +9,7 @@ import sys
 SERVICE_MODULES = frozenset({
     'games.blank.server', 'games.ff7.server', 'games.ff7r.server', 'games.ff7r.themed_server',
     'games.ff8.server', 'games.ff9.server', 'games.rdr.server', 'games.rdr2.server',
-    'games.warband.server',
+    'games.terraria.server', 'games.warband.server',
 })
 
 
