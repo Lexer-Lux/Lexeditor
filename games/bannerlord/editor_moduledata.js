4ef2d2dd4f1557834bf433927564c567836bab83
Lexer <89482099+Lexer-Lux@users.noreply.github.com>
2026-09-19 18:24:11 -0600

    Name Bannerlord required-attribute control

--- games/bannerlord/editor_moduledata.js
+++ games/bannerlord/editor_moduledata.js
@@ -123,7 +123,7 @@ function missingRequiredControl(attribute){
     type:"checkbox",checked:!!attribute.add,title:"Add this XSD-required attribute on Save",
     onchange:event=>{attribute.add=event.target.checked;render();refresh()}
   });
-  return el("div",{class:"bl-control"},toggle,missingRequiredValueControl(attribute));
+  return el("div",{class:"bannerlord-required-control"},toggle,missingRequiredValueControl(attribute));
 }
 
 function moduleDataAttributeLabel(attribute){
